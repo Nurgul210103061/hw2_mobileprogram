c2e8211ff66f9545376ffeb62f08b1ba5ece5052
Nurgul210103061 <210103061@stu.sdu.edu.kz>
2023-10-15 03:01:42 +0300

    first comm

---
+++ app/src/main/java/com/example/loginscreen/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.loginscreen
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var usernameEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        usernameEditText = findViewById(R.id.usernameEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        loginButton = findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+        }
+    }
+}
